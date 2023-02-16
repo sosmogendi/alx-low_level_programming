bc4407f7bbcde79e0d58d234477f1da46cace87f
Sospeter <sosmogendi@gmail.com>
2023-02-16 14:07:07 -0800

    101-quote.c git commit -m 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - entry point to print std error
- *
  * Return: 1
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
-
 }
