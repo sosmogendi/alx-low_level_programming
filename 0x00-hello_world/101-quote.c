251c300a41f1cf45cff1f88e7c6bd6ac5e147675
Sospeter <sosmogendi@gmail.com>
2023-02-16 14:13:49 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry point to print std error
+ * main - entry
  * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
