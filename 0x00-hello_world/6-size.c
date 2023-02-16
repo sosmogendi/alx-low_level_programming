89c7f33c6f54192b7496faa6119dbc526a537ac9
Sospeter <sosmogendi@gmail.com>
2023-02-16 12:29:07 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,15 @@
 int main(void)
 {
 	int i;
-	double d;
+	long int x;
+	long long int y;
+	float d;
 	char c;
 
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of long int is: %lu.\n", (unsigned long)sizeof(x));
+	printf("The size of long long int is: %lu.\n", (unsigned long)sizeof(y));
+	printf("The size of float is: %lu.\n", (unsigned long)sizeof(d));
 	printf("The size of char is: %lu.\n", (unsigned long)sizeof(c));
 
 	return (0);
