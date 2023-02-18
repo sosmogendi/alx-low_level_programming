12d55404ec2205a886e7a7d57f945826444a4af4
Sospeter <sosmogendi@gmail.com>
2023-02-17 16:05:43 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,28 @@
 
 int main(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i + 1; j <= 9; j++)
+		{
+			if (i !=j)
+		
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if (i == 9 && j == 9)
+
+			if (i == 8 && j == 9)
 				continue;
 
 			putchar(',');
 			putchar(' ');
 		}
+		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
