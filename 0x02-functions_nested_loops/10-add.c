2db2abb3cc89485e420fadfa25aee0e979b62847
Sospeter <sosmogendi@gmail.com>
2023-02-21 16:07:25 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: first parameter
+ * @b: second pameter to be added
+ * Return: integer value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
