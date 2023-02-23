41e1418f387630cc4853476469e7133351b9f31e
Sospeter <sosmogendi@gmail.com>
2023-02-23 12:42:15 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: parameter
+ * Result: 0
+ */
+
+void print_line(int n)
+{
+	int count = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (count < n)
+		{
+			_putchar('_');
+			count++;
+		}
+		_putchar('\n');
+	}
+}
