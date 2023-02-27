cfe1d9e9c02edb5d0554fb2d39bf55ec95a63205
Sospeter <sosmogendi@gmail.com>
2023-02-27 14:39:09 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,25 @@
 
 void puts_half(char *str)
 {
-	int count = 0;
+	int a = 0;
+	int b;
 
-	while (*s != '\n')
+	while (str[a] != '\0')
 	{
-		count++;
-		s++;
+		a++;
+	}
+	if (a % 2 == 1)
+	{
+		b = (a - 1) / 2;
+		b += 1;
+	}
+	else
+	{
+		b = a / 2;
+	}
+	for (; b < a; b++;)
+	{
+		_putchar(str[b]);
 	}
 	_putchar('\n');
 }
