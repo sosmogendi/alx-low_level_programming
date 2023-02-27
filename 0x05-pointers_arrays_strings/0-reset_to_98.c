2294631e90e9ded73f8414fe1edf40484327d82b
Sospeter <sosmogendi@gmail.com>
2023-02-27 11:47:44 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - points to an int and updates it value
+ * to 98
+ * @n: input
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	int *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# **0x05. C - Pointers, arrays and strings**
+> * This directory contains files of differnt tasks on pointers, arrays and strings.
