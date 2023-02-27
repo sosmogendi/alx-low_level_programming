69dfc9884b3d348f8e3248108d255d51f25e2e50
Sospeter <sosmogendi@gmail.com>
2023-02-27 13:24:16 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	char rudi = s[0];
+	int count = 0;
+	int x;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (x = 0; x < count; x++)
+	{
+		count--;
+		rudi = s[count];
+		s[count] = rudi;
+	}
+
+}
