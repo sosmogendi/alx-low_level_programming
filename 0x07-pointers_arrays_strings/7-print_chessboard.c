091a1315689d1811b25dda0509869f92be605ad7
Sospeter <sosmogendi@gmail.com>
2023-03-06 09:53:28 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: input
+ * Return: chessboard
+ */
+
+
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+			_putchar('\n');
+		}
+	}
+}
