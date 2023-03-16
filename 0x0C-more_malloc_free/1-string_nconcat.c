c7aebb4e9f6abeafa133f47433ecc17887bf2dc9
Sospeter <sosmogendi@gmail.com>
2023-03-16 15:57:13 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,10 +14,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1 = strlen(s1);
 	unsigned int len2 = strlen(s2);
-	unsigned int len = (n < len2) ? n : len2; /* Maximum length to copy from s2*/
+	/*unsigned int len = (n < len2) ? n : len2;  Maximum length to copy from s2*/
 	char *result;
 
-	result = (char *) malloc(len1 + len + 1); /* Allocate memory for result*/
+	/* Handle null inputs as empty strings*/
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	/* Use the entire s2 if n is greater than or equal to its length*/
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	result = malloc(len1 + n + 1); /* Allocate memory for result*/
 
 	if (result == NULL)
 	{
@@ -28,10 +44,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	memcpy(result, s1, len1);
 
 	/* Copy at most n characters from s2 into result*/
-	memcpy(result + len1, s2, len);
+	memcpy(result + len1, s2, n);
 
 	/* Add null terminator to result*/
-	result[len1 + len] = '\0';
+	result[len1 + n] = '\0';
 
 	return (result);
 }
