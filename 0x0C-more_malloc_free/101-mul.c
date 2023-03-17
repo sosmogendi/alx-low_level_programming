397a2d4ef457e2cda1dd2acae0e9644c60ebff79
Sospeter <sosmogendi@gmail.com>
2023-03-16 17:31:16 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * is_valid_number - checks for a valid number
+ * @num: input to check
+ * Return: 0 or 1
+ */
+
+int is_valid_number(char *num)
+{
+	long unsigned int i;
+
+	for (i = 0; i < strlen(num); i++)
+	{
+		if (!isdigit(num[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * multiply - multiplies two positive numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: result
+ */
+
+int multiply(char *num1, char *num2)
+{
+	int result = atoi(num1) * atoi(num2);
+
+	return (result);
+}
+
+/**
+ * main - entry point
+ * @argc: number of inputs
+ * @argv: pointwr to array
+ * Return: result of multiplication
+ */
+
+int main(int argc, char **argv)
+{
+	char *num1, *num2;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = argv[1];
+	num2 = argv[2];
+
+	if (!is_valid_number(num1) || !is_valid_number(num2))
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	result = multiply(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
