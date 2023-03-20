f4d40542afad24ba934b7104437b2a621fa1debe
Sospeter <sosmogendi@gmail.com>
2023-03-20 15:11:48 -0700

    Creates a new dog/variable

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of new dog
+ * @age: the dog's age
+ * @owner: dog's owner
+ * Description: this function creates new dog and
+ * all its elements
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *myDog = malloc(sizeof(dog_t));
+
+	if (myDog == NULL)
+		return (NULL);
+
+	/* copying name  */
+	myDog->name = malloc(strlen(name) + 1);
+	if (myDog == NULL)
+	{
+		free(myDog);
+		return (NULL);
+	}
+	strcpy(myDog->name, name);
+
+	/* copying owner  */
+	myDog->owner = malloc(strlen(owner) + 1);
+	if (myDog->owner == NULL)
+	{
+		free(myDog);
+		return (NULL);
+	}
+	strcpy(myDog->owner, owner);
+
+	myDog->age = age;
+
+	return (myDog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,7 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *another_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 
 
