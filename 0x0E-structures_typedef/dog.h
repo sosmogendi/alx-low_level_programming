b7dd1fec56e2b23686f5a0d873d921b112a93b46
Sospeter <sosmogendi@gmail.com>
2023-03-20 14:44:27 -0700

    New type as a new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,15 +9,17 @@
  * Description: This is a new struct
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *another_dog(char *name, float age, char *owner);
+
 
 
 #endif
