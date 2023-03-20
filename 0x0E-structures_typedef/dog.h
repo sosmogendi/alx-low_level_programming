26c5ac9fac42f9d1412ce87e21a65a8808c287c1
Sospeter <sosmogendi@gmail.com>
2023-03-20 12:30:59 -0700

    New type struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define DOG(name, age, owner)
+/**
+ * struct dog - new type dog struct
+ * @name: 1st element
+ * @age: 2nd element
+ * @owner: 3rd element
+ * Description: This is a new struct
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 #endif
