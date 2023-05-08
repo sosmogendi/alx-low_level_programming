b5157d9835151895c9ba12947e09a6612df7addc
Sospeter <sosmogendi@gmail.com>
2023-05-08 14:26:12 -0700

    ELF header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,81 @@
+#include "main.h"
+
+/**
+ * print_error_msg - prints an error message to stderr and exits with code 98
+ * @error_msg: the error message to print
+ */
+
+void print_error_msg(const char *error_msg)
+{
+	fprintf(stderr, "%s\n", error_msg);
+	exit(98);
+}
+
+/**
+ * print_elf_header_info - prints information from an ELF header
+ * @header: pointer to the ELF header
+ */
+
+void print_elf_header_info(const Elf64_Ehdr *header)
+{
+	int x;
+
+	printf("  Magic:   ");
+	for (x = 0; x < EI_NIDENT; x++)
+	{
+		printf("%02x ", header->e_ident[x]);
+	}
+	printf("\n");
+
+	printf("  Class: %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ?
+	"ELF32" : "ELF64");
+	printf("  Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ?
+	"2's complement, little endian" : "2's complement, big endian");
+	printf("  Version: %d\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI: %d\n", header->e_ident[EI_OSABI]);
+	printf("  ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type: %u\n", header->e_type);
+	printf("  Entry point address: %#lx\n", header->e_entry);
+}
+
+/**
+ * main - displays information about the ELF header of a file
+ * @argc: the number of arguments
+ * @argv: an array of arguments
+ *
+ * Return: 0 on success, 98 on failure
+ */
+
+int main(int argc, char **argv)
+{
+	int fd;
+	Elf64_Ehdr header;
+
+	if (argc != 2)
+	{
+		print_error_msg("Usage: elf_header elf_filename");
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		print_error_msg("Error: Cannot open the file");
+	}
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	{
+		print_error_msg("Error: Cannot read the file");
+	}
+
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+			header.e_ident[EI_MAG1] != ELFMAG1 ||
+			header.e_ident[EI_MAG2] != ELFMAG2 ||
+			header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		print_error_msg("Error: Not an ELF file");
+	}
+	print_elf_header_info(&header);
+	if (close(fd) == -1)
+	{
+		print_error_msg("Error: Cannot close the file");
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <elf.h>
+
 
 #define BUFFER_SIZE 1024
 
@@ -14,7 +16,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int main(int argc, char *argv[]);
-
+void print_error_msg(const char *error_msg);
+void print_elf_header_info(const Elf64_Ehdr *header);
 
 #endif
