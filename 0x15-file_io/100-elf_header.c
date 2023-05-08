9ee34176ce8e7d550e2046c52a6970bab65964ce
Sospeter <sosmogendi@gmail.com>
2023-05-08 15:46:56 -0700

    ELF header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
+
 /**
- * print_error_msg - prints an error message to stderr and exits with code 98
+ * print_error_msg - prints an error message to
+ * stderr and exits with code 98
  * @error_msg: the error message to print
  */
 
 void print_error_msg(const char *error_msg)
 {
-	fprintf(stderr, "%s\n", error_msg);
+	fprintf(stderr, "Error: %s\n", error_msg);
 	exit(98);
 }
 
@@ -20,28 +22,68 @@ void print_elf_header_info(const Elf64_Ehdr *header)
 {
 	int x;
 
+	printf("ELF Header:\n");
 	printf("  Magic:   ");
 	for (x = 0; x < EI_NIDENT; x++)
 	{
 		printf("%02x ", header->e_ident[x]);
 	}
 	printf("\n");
+	printf("  Class:                             %s\n",
+	header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
 
-	printf("  Class: %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ?
-	"ELF32" : "ELF64");
-	printf("  Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ?
-	"2's complement, little endian" : "2's complement, big endian");
-	printf("  Version: %d\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI: %d\n", header->e_ident[EI_OSABI]);
-	printf("  ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type: %u\n", header->e_type);
-	printf("  Entry point address: %#lx\n", header->e_entry);
+	printf("  Data:                              %s\n",
+	header->e_ident[EI_DATA] == ELFDATA2LSB ?
+	"2's complement, little endian" :
+	"2's complement, big endian");
+
+	printf("  Version:                           %d\n",
+	header->e_ident[EI_VERSION]);
+
+	printf("  OS/ABI:                            %s\n",
+	header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
+	header->e_ident[EI_OSABI] == ELFOSABI_HPUX ? "UNIX - HP-UX" :
+	header->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
+	header->e_ident[EI_OSABI] == ELFOSABI_LINUX ? "UNIX - Linux" :
+	header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
+	header->e_ident[EI_OSABI] == ELFOSABI_IRIX ? "UNIX - IRIX" :
+	header->e_ident[EI_OSABI] == ELFOSABI_FREEBSD ? "UNIX - FreeBSD" :
+	header->e_ident[EI_OSABI] == ELFOSABI_TRU64 ? "UNIX - TRU64" :
+	header->e_ident[EI_OSABI] == ELFOSABI_ARM ? "ARM" :
+	header->e_ident[EI_OSABI] == ELFOSABI_STANDALONE ? "Standalone App" :
+	"<unknown>");
+
+	printf("  ABI Version:                       %d\n",
+	header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              ");
+	switch (header->e_type)
+	{
+		case ET_NONE:
+			printf("NONE (No file type)\n");
+			break;
+		case ET_REL:
+			printf("REL (Relocatable file)\n");
+			break;
+		case ET_EXEC:
+			printf("EXEC (Executable file)\n");
+			break;
+		case ET_DYN:
+			printf("DYN (Shared object file)\n");
+			break;
+		case ET_CORE:
+			printf("CORE (Core file)\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", header->e_type);
+	}
+	printf("  Entry point address:               %#lx\n",
+	(unsigned long) header->e_entry);
 }
 
 /**
  * main - displays information about the ELF header of a file
  * @argc: the number of arguments
- * @argv: an array of arguments
+  * @argv: an array of arguments
  *
  * Return: 0 on success, 98 on failure
  */
@@ -64,7 +106,6 @@ int main(int argc, char **argv)
 	{
 		print_error_msg("Error: Cannot read the file");
 	}
-
 	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
 			header.e_ident[EI_MAG1] != ELFMAG1 ||
 			header.e_ident[EI_MAG2] != ELFMAG2 ||
