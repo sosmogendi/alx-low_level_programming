49b68bc5f7bd6c14a2990e416b37a5182c87f24e
Sospeter <sosmogendi@gmail.com>
2023-10-11 12:52:46 -0700

    Exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,6 +1,43 @@
 #include "search_algos.h"
 
 /**
+ * binary_search - Searches for a value in a sorted array using Binary search
+ * @array: Pointer to the first element of the array
+ * @low: The starting index in the array
+ * @high: The ending index in the array
+ * @value: Value to search for
+ *
+ * Return: Index where value is located, or -1 if not found
+ */
+int binary_search(int *array, size_t low, size_t high, int value)
+{
+	size_t mid;
+	int mid_value;
+	size_t i;
+
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		mid_value = array[mid];
+
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+		}
+		printf("\n");
+		if (mid_value == value)
+			return (mid);
+		if (mid_value < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+	return (-1);
+}
+/**
  * exponential_search - Searches for a value in a sorted array
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
@@ -11,20 +48,18 @@
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t bound;
-	int res;
 
 	if (array == NULL)
 		return (-1);
-
 	if (array[0] == value)
 		return (0);
+
 	bound = 1;
 	while (bound < size && array[bound] < value)
 	{
-	printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
-	bound *= 2;
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", bound / 2, (bound < size) ? bound : size - 1);
-	res = binary_search(array, bound / 2, (bound < size) ? bound : size, value);
-	return (res);
+	return (binary_search(array, bound / 2, (bound < size) ? bound : size - 1, value));
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,13 +2,13 @@
 #define SEARCH_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t low, size_t high, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-
 
 #endif
